9c0a59dddc3f1295d96ff33e1440f393224f3d16
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-09-21 18:09:06 +0200

    stuff.

---
+++ MachineLearningPractice/Models/BoundingBox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MachineLearningPractice.Models
+{
+    class BoundingBox
+    {
+        public Size Size { get; set; }
+        public Point Location { get; set; }
+    }
+}
