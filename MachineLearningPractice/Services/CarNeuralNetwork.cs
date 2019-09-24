8cc2a30f3671bd7611f88192e01688abe7705008
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-09-24 17:46:53 +0200

    stuff.

--- MachineLearningPractice/Services/CarNeuralNetwork.cs
+++ MachineLearningPractice/Services/CarNeuralNetwork.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Statistics.Kernels;
+using Microsoft.ML;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,26 @@ using System.Threading.Tasks;
 
 namespace MachineLearningPractice.Services
 {
+    struct TrainingInstruction
+    {
+        public CarSensorReading CarSensorReading { get; set; }
+        public CarSimulationTick CarResponse { get; set; }
+    }
+
     class CarNeuralNetwork
     {
+        private SequentialMinimalOptimization<Gaussian> teacher;
+        private HashSet<(CarSensorReading)>
+
+        public CarNeuralNetwork()
+        {
+            this.teacher = new SequentialMinimalOptimization<Gaussian>()
+            {
+                UseComplexityHeuristic = true,
+                UseKernelEstimation = true
+            };
+        }
+
         public void Train(
             CarSensorReading sensorReading,
             CarSimulationTick carSimulationTick)
